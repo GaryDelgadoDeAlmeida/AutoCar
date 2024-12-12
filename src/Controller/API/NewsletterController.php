0fa8c97da5b3510666b626ce087ba6f7e9c78669
Garry ALMEIDA <gary.almeida.work@gmail.com>
2024-12-12 23:27:28 +0100

    Fix undefined repository

--- src/Controller/API/NewsletterController.php
+++ src/Controller/API/NewsletterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\API;
 
 use App\Manager\NewsletterManager;
+use App\Repository\NewsletterRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
